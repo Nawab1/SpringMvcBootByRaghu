1cdb160402d68329d4201d187695ef312faf151b
dev <dev@gmail>
2021-06-03 08:05:34 +0530

    All testing is fixed

--- SpringBootMvcGit/src/main/java/com/example/demo/productservice/ProductServiceImpl.java
+++ SpringBootMvcGit/src/main/java/com/example/demo/productservice/ProductServiceImpl.java
@@ -4,13 +4,21 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.javabean.Product;
 import com.example.demo.repository.IProductRepository;
 
 import lombok.AllArgsConstructor;
-@AllArgsConstructor
+import lombok.NoArgsConstructor;
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Service
 public class ProductServiceImpl implements IProductService {
 
+	@Autowired
 private IProductRepository productRepo;
 
 @Transactional
--- SpringBootMvcGit/src/main/java/com/example/demo/repository/IProductRepository.java
+++ SpringBootMvcGit/src/main/java/com/example/demo/repository/IProductRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.javabean.Product;
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Integer>{
+	@Query("select p from Product p where p.pId=:id")
    Product getProductById(Integer id);
+	
+	
+	
+	
 }
