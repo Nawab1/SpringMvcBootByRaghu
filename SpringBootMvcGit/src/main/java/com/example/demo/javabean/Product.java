c7413c1eee044a22cb90076fb7d7085486311935
dev <dev@gmail>
2021-06-03 06:27:46 +0530

    javaBean works completed,i merged it.

---
+++ SpringBootMvcGit/src/main/java/com/example/demo/javabean/Product.java
@@ -0,0 +1,21 @@
+package com.example.demo.javabean;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+@Data
+@Entity
+@Table(name="PRODUCTTAB")
+public class Product implements Serializable{
+@Id
+private Integer pId;
+private String pCode;
+private String pName;
+private double pCost;
+private String gst;
+private String note;
+}
