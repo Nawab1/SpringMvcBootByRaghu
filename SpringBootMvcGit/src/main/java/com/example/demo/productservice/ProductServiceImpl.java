4781537808a4afd571d0f1f9bcb4aca4d105fd73
dev <dev@gmail>
2021-06-03 07:03:13 +0530

    productServiceImpl parially done

--- SpringBootMvcGit/src/main/java/com/example/demo/productservice/ProductServiceImpl.java
+++ SpringBootMvcGit/src/main/java/com/example/demo/productservice/ProductServiceImpl.java
@@ -1,5 +1,40 @@
 package com.example.demo.productservice;
 
-public class ProductServiceImpl {
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import com.example.demo.javabean.Product;
+import com.example.demo.repository.IProductRepository;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+public class ProductServiceImpl implements IProductService {
+
+private IProductRepository productRepo;
+
+@Transactional
+public Integer saveProduct(Product p) {
+	p =  productRepo.save(p);
+	return p.getPId();
+}
+
+@Override
+public List<Product> getAllProducts() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public void deleteProduct(Integer id) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public Product getProductById(Integer id) {
+	// TODO Auto-generated method stub
+	return null;
+}
+	
 }
